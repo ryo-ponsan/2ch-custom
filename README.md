4b46aad751dae32b36aa7419d5a61d6843ee3ad8
ryo-ponsan <ryo.pon.san@gmail.com>
2024-11-19 22:39:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 2ちゃんねる掲示板カスタム
+- 2ちゃんねる＋AI機能搭載した簡易サイト
+- jQueryを使ったフロント、 PHPを使ったバックを使ってみる勉強用リポジトリ
+
+## 技術スタック
+- jQuery
+- HTML
+- CSS, Bootstrap
+- PHP
+- MySQL
+
+## 参考
+- [Bootstrapチートシート](https://getbootstrap.com/docs/5.0/examples/cheatsheet/)
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2ch掲示板</title>
+</head>
+<body>
+    <header>
+        <h1 class="title">2ちゃんねる掲示板</h1>
+        <hr>
+    </header>
+    <div>
+        <dev class="childwrapper">
+            <div class="threadTitle">
+                <span>【タイトル】</span>
+                <h1>2チャンネル掲示板作ってみた</h1>
+            </div>
+            <section>
+                <div class="wrapper">
+                    <span>名前：</span>
+                    <p class="username">ponsan</p>
+                    <time>:2024/11/19 20:00</time>
+                </div>
+            </section>
+        </dev>
+    </div>
+</body>
+</html>
