c553cc604123e6e9c57ea0ae9919ca90aaf646ce
ryo-ponsan <ryo.pon.san@gmail.com>
2024-11-19 23:11:41 +0900

    add frame html and css

--- index.php
+++ index.php
@@ -4,26 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>2ch掲示板</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
     <header>
-        <h1 class="title">2ちゃんねる掲示板</h1>
+        <h1 class="title">2ちゃんねるAI掲示板</h1>
         <hr>
     </header>
-    <div>
-        <dev class="childwrapper">
+    <div class="threadWrapper">
+        <div class="childWrapper">
             <div class="threadTitle">
                 <span>【タイトル】</span>
-                <h1>2チャンネル掲示板作ってみた</h1>
+                <h1>2チャンネルAI掲示板作ってみた</h1>
             </div>
             <section>
-                <div class="wrapper">
-                    <span>名前：</span>
-                    <p class="username">ponsan</p>
-                    <time>:2024/11/19 20:00</time>
-                </div>
+                <article>
+                    <div class="wrapper">
+                        <div class="nameArea">
+                            <span>名前：</span>
+                            <p class="username">ponsan</p>
+                            <time>:2024/11/19 20:00</time>
+                        </div>
+                        <p class="comment">ハードコメント</p>
+                    </div>
+                </article>
             </section>
-        </dev>
+        </div>
     </div>
 </body>
 </html>
